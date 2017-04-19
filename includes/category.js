f6a93f9f8cd9f172b31b1d38df3cfffb639d8ef3
thanel@redletra.com <thanel@redletra.com>
2017-04-19 21:45:42 +1000

    add category

---
+++ includes/category.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// create a schema
+var categorySchema = new Schema({
+  name: String
+});
+
+// the schema is useless so far
+// we need to create a model using it
+var Category = mongoose.model('Category', categorySchema);
+
+// make this available to our users in our Node applications
+module.exports = Category;
--- includes/mongo-functions.js
+++ includes/mongo-functions.js
@@ -1,16 +1,34 @@
 var assert = require('assert');
+var Category = require('./category');
 
 module.exports = {
-  insertCategory: function (req, resp, db) {
+  insertCategory: function (req, resp, db, callback) {
     console.log('tom');
     console.log(req.body);
-    db.collection('categories').insertOne(
-        req.body
-    , function(err, result) {
-        assert.equal(err, null);
-        console.log("Inserted a document into the categories collection.");
+
+    var categoryExists = false;
+    //does the category already exist?
+    
+    var findPromise = Category.find(req.body).exec();
+
+    findPromise.then(function(category){
+        console.log('then ', category);
+        if(category.length>0){
+            console.log('return false 1');
+            callback(true);
+        }else{
+            console.log('save');
+            var newCategory = new Category(req.body);
+            var savePromise = newCategory.save();
+            console.log('save then', savePromise);
+            savePromise.then(function(category){
+                console.log('return true 1');
+                callback(false);
+            });
+        }
     });
 
+    
   },
 
   listCategories: function( db, callback){
